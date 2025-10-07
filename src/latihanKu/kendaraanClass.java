86754cbd327c59f27d51d6ee8fd6c211b6ffd4f2
aufidhuha <aufikawanit@gmail.com>
2025-10-07 14:57:30 +0700

    tambah parent

---
+++ src/latihanKu/kendaraanClass.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanKu;
+
+/**
+ *
+ * @author ASUS
+ */
+public class kendaraanClass {
+    
+    public String merk, tipe;
+    public int tahun_produksi, harga;
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public int getTahun_produksi() {
+        return tahun_produksi;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+    
+    
+    
+}
